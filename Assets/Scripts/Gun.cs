d8058d5da0dcfeb2e7e0fb34e07633d39d667d58
Wout Tessens <wout.tessens@student.kuleuven.be>
2024-10-18 14:29:38 +0200

    movement cube

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public int maxBullets;
+    public int currentBullets;
+    public enum WeaponType
+    {
+        Shotgun,
+        P90
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        transform.Translate(direction * 25 * Time.deltaTime);
+    }
+}
