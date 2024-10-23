c1d4e8d6f9e6ca14022255a2353c1ab1ad6865ad
Maxim Vanweddingen <maximvanweddingen@proton.me>
2024-10-23 14:16:37 +0200

    Text

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,26 +1,92 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Collections;  // Required for IEnumerator
 using UnityEngine;
+using UnityEngine.Networking;  // For HTTP requests
+using UnityEngine.UI;  // For UI components like Text
+using TMPro;
 
 public class Gun : MonoBehaviour
 {
-    public int maxBullets;
+    public int maxBullets = 30;
     public int currentBullets;
     public WeaponType weaponType;
+    public TMP_Text ammoText;  // Reference to the UI Text for ammo
+
     public enum WeaponType
     {
         Shotgun,
         P90
     }
-    // Start is called before the first frame update
+
+    private string esp32IP = "http://<ESP32_IP_ADDRESS>/update";  // Replace with your ESP32 IP address
+
     void Start()
     {
-        
+        currentBullets = maxBullets;  // Initialize current bullets
+        UpdateAmmoText();  // Update the UI with the initial ammo
+        UpdateESP32();  // Send the initial ammo to ESP32
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Shoot when the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0))  // Left mouse button for shooting
+        {
+            Shoot();
+        }
+
+        // Reload when the 'R' key is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+    }
+
+    public void Shoot()
+    {
+        if (currentBullets > 0)
+        {
+            currentBullets--;  // Decrease bullet count when shooting
+            UpdateAmmoText();  // Update the ammo UI text
+            UpdateESP32();  // Send updated ammo count to ESP32
+        }
+        else
+        {
+            Debug.Log("Out of ammo!");
+        }
+    }
+
+    public void Reload()
+    {
+        currentBullets = maxBullets;  // Reload the weapon to max bullets
+        UpdateAmmoText();  // Update the ammo UI text
+        UpdateESP32();  // Send updated ammo count to ESP32
+    }
+
+    void UpdateAmmoText()
+    {
+        ammoText.text = "Ammo: " + currentBullets;  // Update the ammo text on UI
+    }
+
+    // Send the current ammo to ESP32
+    void UpdateESP32()
+    {
+        string url = esp32IP + "?health=" + 100 + "&ammo=" + currentBullets;  // Assuming health is fixed at 100 for now
+        StartCoroutine(SendData(url));
+    }
+
+    // Coroutine to send HTTP GET request to ESP32
+    IEnumerator SendData(string url)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log("ESP32 updated with ammo: " + currentBullets);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Collections;  // Required for IEnumerator
 using UnityEngine;
+using UnityEngine.UI;  // For UI components like Text
+using UnityEngine.Networking;  // For HTTP requests
+using TMPro;
 
 public class Player : MonoBehaviour
 {
-    public int maxHP;
+    public int maxHP = 100;
     public int currentHP;
     public float speed;
-    // Start is called before the first frame update
+    public TMP_Text healthText;  // Reference to the UI Text for health
+
+    private string esp32IP = "http://<ESP32_IP_ADDRESS>/update";  // Replace with your ESP32 IP address
+
     void Start()
     {
-
+        currentHP = maxHP;
+        UpdateHealthText();  // Update the UI with the initial health
+        UpdateESP32();  // Send the initial health to ESP32
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
-
         transform.Translate(direction * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        if (currentHP < 0) currentHP = 0;
+
+        UpdateHealthText();  // Update the health on UI
+        UpdateESP32();  // Send updated health to ESP32
+    }
+
+    void UpdateHealthText()
+    {
+        healthText.text = "Health: " + currentHP;  // Update the health text
+    }
+
+    // Send the current health to ESP32
+    void UpdateESP32()
+    {
+        string url = esp32IP + "?health=" + currentHP + "&ammo=" + 50;  // Assuming ammo is 50 for now
+        StartCoroutine(SendData(url));
+    }
+
+    // Coroutine to send HTTP GET request to ESP32
+    IEnumerator SendData(string url)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log("ESP32 updated with health: " + currentHP);
+        }
     }
 }
