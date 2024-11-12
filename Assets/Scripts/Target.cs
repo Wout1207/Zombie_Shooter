ed7de6d6afa5b002976e4ef005461afe7cdf2e7d
Wout Tessens <wout.tessens@student.kuleuven.be>
2024-11-12 15:50:13 +0100

    improved walls + target ai

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Target : MonoBehaviour
 {
     //public static Action OnTargetHit;
-
+    private GameObject player;
+    private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null)
+        {
+            agent = gameObject.AddComponent<NavMeshAgent>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if ((transform.position-player.transform.position).magnitude < 30)
+        {
+            agent.SetDestination(player.transform.position);
+        }
     }
     public void Hit()
     {
