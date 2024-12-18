abf0d29a9ac76362d03f42493ffcab08ca39e91b
Maxim Vanweddingen <maximvanweddingen@proton.me>
2024-12-18 20:46:40 +0100

    Immune and the right zombies

--- Assets/Scripts/FireResistantZombie.cs
+++ Assets/Scripts/FireResistantZombie.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FireResistantZombie : Target
 {
     // Override the Hit method to make it resistant to normal bullets
     public override void Hit(float damage)
     {
-        Debug.Log("Zombie 5 is resistant to normal bullets!");
+        ShowImmunityText("IMMUNE");
+        /*Debug.Log("FireResistantZombie Hit method called.");
+        Debug.Log("Zombie 5 is resistant to normal bullets!");*/
     }
 
     public override void fireHit(float damage)
     {
         base.fireHit(damage);
-        Debug.Log("Zombie 5 takes fire damage!");
+        //Debug.Log("Zombie 5 takes fire damage!");
+    }
+
+    private void ShowImmunityText(string message)
+    {
+        GameObject text = Instantiate(damageText, transform);
+        text.transform.LookAt(player.transform);
+        text.transform.position += new Vector3(0, 2, 0); 
+        text.transform.Rotate(0, 180, 0);
+
+        TMP_Text tmp = text.GetComponent<TMP_Text>();
+        if (tmp != null)
+        {
+            tmp.text = message;
+            tmp.color = Color.yellow;
+        }
     }
 }
 
